d1c9dcb8bd4399022192a1b6b9f698615cfe70a5
micraj <mrajzer2004@gmail.com>
2025-06-08 12:00:40 +0100

    Add vite preprocess

--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -1,7 +1,9 @@
 import adapter from '@sveltejs/adapter-node';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 export default {
   kit: {
+    preprocess: vitePreprocess(),
     adapter: adapter({
       out: 'build'  // output folder for the Node server build
     })
