b317ec501c91510b7f47acc986dd8c27c16f2348
micraj <mrajzer2004@gmail.com>
2025-07-07 18:45:01 +0100

    Seperate tinymce editor and post editor

--- frontend/src/lib/components/TinyEditor.svelte
+++ frontend/src/lib/components/TinyEditor.svelte
@@ -3,8 +3,6 @@
 	import { onMount, onDestroy } from 'svelte';
 
 	let editorId = 'my-tinymce-editor';
-	let title: string = '';
-	let cover_image_path: string = '';
 
 	export let content: string = '';
 	export let height: number = 300;
@@ -55,84 +53,6 @@
 			});
 		}
 	});
-
-	function handleSubmit() {
-		console.log('Submitted Title:', title);
-		console.log('Submitted Cover Image:', cover_image_path);
-		console.log('Submitted Content:', content);
-		fetch('http://127.0.0.1:8000/posts/create_post', {
-			method: 'POST',
-			headers: {
-				'Content-type': 'application/json; charset=UTF-8'
-			},
-			body: JSON.stringify({
-				title: title,
-				content: content,
-				cover_image: cover_image_path
-			})
-		})
-			.then((response) => response.json())
-			.then((data) => {
-				console.log('Response from server:', data);
-			})
-			.catch((error) => {
-				console.error('Error submitting content:', error);
-			});
-	}
 </script>
 
-{#if browser}
-	<div class="editor-container">
-		<input type="text" bind:value={title} placeholder="Enter title" class="title-input" />
-		<input
-			type="text"
-			bind:value={cover_image_path}
-			placeholder="Enter cover image path"
-			class="title-input"
-		/>
-
-		<textarea id={editorId}></textarea>
-
-		<button on:click={handleSubmit} class="submit-button">Submit</button>
-	</div>
-{:else}
-	<p>Editor loading...</p>
-{/if}
-
-<style>
-	.editor-container {
-		max-width: 60%;
-		margin: 2rem auto;
-		padding: 1rem;
-		display: flex;
-		flex-direction: column;
-		gap: 1rem;
-	}
-
-	.title-input {
-		padding: 0.5rem;
-		font-size: 1.5rem;
-		border: 1px solid #ccc;
-		border-radius: 4px;
-		width: 100%;
-	}
-
-	.submit-button {
-		align-self: flex-start;
-		padding: 0.6rem 1.2rem;
-		font-size: 1rem;
-		background-color: #007bff;
-		color: white;
-		border: none;
-		border-radius: 4px;
-		cursor: pointer;
-	}
-
-	.submit-button:hover {
-		background-color: #0056b3;
-	}
-
-	:global(.tox) {
-		max-width: 100%;
-	}
-</style>
+<textarea id={editorId}></textarea>
--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,11 +1,12 @@
 <script lang="ts">
 	import PostList from '$lib/components/PostList.svelte';
 	import TinyEditor from '$lib/components/TinyEditor.svelte';
+	import PostEditor from '$lib/components/PostEditor.svelte';
 	let content: string = '<p>Hello, TinyMCE!</p>';
 </script>
 
 <h1>My Editor</h1>
-<TinyEditor bind:content height={400} />
+<PostEditor />
 <PostList />
 
 <!-- <h2>Preview:</h2>
